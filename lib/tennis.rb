fedf9abc7f4d4a4c90df83448960fb3546d7be42
Brian O'Sullivan <brian.osullivan@live.com>
2015-05-13 00:43:54 +0100

    Generalizing implementation of scoring

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,17 +1,24 @@
 class Tennis
 
+  SCORING = ['Love','Fifteen','Thirty','Forty']
+
   def initialize player1, player2
     @player1 = player1
     @player2 = player2
   end
 
   def score
-    if @player1.points == 1 and @player2.points == 0
-      return "Fifteen-Love"
+
+    if points_played > 0
+      return "#{SCORING[@player1.points]}-#{SCORING[@player2.points]}"
     end
-    if @player1.points == 2 and @player2.points == 0
-      return "Thirty-Love"
-    end
-    return "Love-All"
+
+    "Love-All"
   end
+
+  def points_played
+    @player1.points + @player2.points
+  end
+
+  private :points_played
 end
